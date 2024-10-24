9ec091545223b0e004939f30d0ae55ca5c4be828
YashShah138 <89223735+YashShah138@users.noreply.github.com>
2024-10-24 12:15:22 -0500

    created outline

---
+++ main.py
@@ -0,0 +1,25 @@
+import numpy as np
+import pandas as pd
+import yfinance as yf
+import matplotlib.pyplot as plt
+
+def monte_carlo_sim(stock_symbol, num_sims, num_days):
+    # Step 1: Data Collection
+
+
+    # Step 2: Data Processing
+
+
+    # Step 3: Simulation Setup
+
+
+    # Step 4: Monte Carlo Simulations
+
+
+    # Step 5: Analysis
+
+
+    # Step 6: Visualization
+
+
+monte_carlo_sim('AAPL', num_sims=10_000, num_days=365)
